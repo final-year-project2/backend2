0c576d0226d960ee93f87c94a60caf0ad7126851
yihenewaddis <yihenewu@gmail.com>
2024-03-24 20:38:39 +0300

    user authentication conplated

--- backend/settings.py
+++ backend/settings.py
@@ -123,8 +123,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'final_year_project',
+        'USER': 'postgres',
+        'PASSWORD': 'bdu1203346',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
