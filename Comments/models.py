206bc3d2c4fca5f6a25ca06c60fddc895480f544
yihenew <yihenewu89@gmail.com>
2024-06-29 00:14:30 +0300

    some chages are added

--- Comments/models.py
+++ Comments/models.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from Product.models import Ticket
 user = settings.AUTH_USER_MODEL
 
-class TicketComment(models.Model):
+class TicketCommentModel(models.Model):
     User_id = models.ForeignKey(user,on_delete=models.CASCADE)
     Ticket_id = models.ForeignKey(Ticket,on_delete=models.CASCADE)
     Comment = models.CharField(null=False,max_length=100)
--- Comments/serializer.py
+++ Comments/serializer.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
-from .models import TicketComment
+from .models import TicketCommentModel
 class TicketCommentSerializer(serializers.ModelSerializer):
     User_id = serializers.SerializerMethodField(read_only = True)
     class Meta:
-        model= TicketComment
+        model= TicketCommentModel
         fields = ['id','User_id','Ticket_id','Comment']
     def get_User_id(self,obj):
         return  obj.User_id.id
